6bb5f29fe94ba70a0b2111fdf32c17387de62db1
Rai Pocius <pocius.raimondas@gmail.com>
2024-02-26 09:50:14 -0600

    Begin styling Instructions component

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -6,21 +6,27 @@ const Instructions: React.FC = () => {
     <div className={styles.instruction_container}>
       <div className={styles.instruction_step}>
         <img src="invite.svg" alt="Invite" />
+        <div className={styles.instruction_text}>
         <p className={styles.blue_text}>STEP 1</p>
         <h2>INVITE FRIENDS</h2>
         <p>Refer friends with your unique referral link.</p>
+        </div>
       </div>
       <div className={styles.instruction_step}>
-        <img src="collect-coins.svg" alt="Collect coins" />
-        <p className={styles.blue_text}>STEP 1</p>
+        <div className={styles.instruction_text}>
+        <p className={styles.blue_text}>STEP 2</p>
         <h2>COLLECT COINS</h2>
         <p>Get 1 coin for each friend that installs our extension using your referral link.</p>
+        </div>
+        <img src="collect-coins.svg" alt="Collect coins" />
       </div>
       <div className={styles.instruction_step}>
         <img src="voucher.svg" alt="Voucher" />
-        <p className={styles.blue_text}>STEP 1</p>
+        <div className={styles.instruction_text}>
+        <p className={styles.blue_text}>STEP 3</p>
         <h2>GET VOUCHER</h2>
         <p>Redeem for a $20 hotel booking voucher once you collect 20 coins.</p>
+        </div>
       </div>
     </div>
   );
