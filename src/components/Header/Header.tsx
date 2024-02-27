e15dafaffe66226064eab134108f2ade050c0bef
Rai Pocius <pocius.raimondas@gmail.com>
2024-02-26 21:45:01 -0600

    Add link to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,14 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header_container}>
-      <img src="logo.svg" alt="Ratepunk logo" className={styles.logo} />
-      <nav className={styles.nav}>
-        <a href="#" className={styles.link} target="_blank" rel="noopener noreferrer">Chrome Extension</a>
-        <a href="#" className={styles.link} target="_blank" rel="noopener noreferrer">Price Comparison</a>
-        <a href="#" className={styles.link} target="_blank" rel="noopener noreferrer">Blog</a>
-      </nav>
+        <a href="https://www.ratepunk.com/" target="_blank" rel="noopener noreferrer">
+          <img src="logo.svg" alt="Ratepunk logo" className={styles.logo} />
+        </a>
+        <nav className={styles.nav}>
+          <a href="#" className={styles.link} target="_blank" rel="noopener noreferrer">Chrome Extension</a>
+          <a href="#" className={styles.link} target="_blank" rel="noopener noreferrer">Price Comparison</a>
+          <a href="#" className={styles.link} target="_blank" rel="noopener noreferrer">Blog</a>
+        </nav>
       </div>
     </header>
   );
