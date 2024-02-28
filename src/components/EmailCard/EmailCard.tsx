8f6ca73515a6b2a5aac4c728ebe69291ffa71e7e
Rai Pocius <pocius.raimondas@gmail.com>
2024-02-27 22:26:08 -0600

    Add referral link input and button logic, and sass

--- src/components/EmailCard/EmailCard.tsx
+++ src/components/EmailCard/EmailCard.tsx
@@ -4,38 +4,71 @@ import styles from './EmailCard.module.sass';
 const EmailCard: React.FC = () => {
     const [email, setEmail] = useState('');
     const [error, setError] = useState('');
+    const [goodEmail, setGoodEmail] = useState(false);
 
-    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;;
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
     const image = "";
 
     const validateEmail = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        if (emailRegex.test(email)) {
-            setError("Your email is confirmed!")
-        } else if (email == "") {
-            setError("Enter your email address");
-        } else if (!emailRegex.test(email)) {
-            setError("This email is not valid");
-        }
+        event.preventDefault();
+
+    if (email === "") {
+        setError("Enter your email address");
+    } else if (emailRegex.test(email)) {
+        setError("");
+        setGoodEmail(true);
+    } else {
+        setError("This email is not valid");
+    }
     };
 
-    return (
+    const referralLink = "https://ratepunk.com/referral"
+
+    const copyLink = (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+      ) => {
+        event.preventDefault();
+        navigator.clipboard.writeText(referralLink);
+      };
+
+      return (
         <div className={styles.email_card}>
             <h1>REFER FRIENDS AND GET REWARDS</h1>
             <p>Refer your friends to us and earn hotel booking vouchers. We'll give you 1 coin for each friend that installs our extension. Minimum cash-out at 20 coins.</p>
-            <div className={styles.error_state}>{error && <div>{error}</div>}</div>
-            <form onSubmit={validateEmail}>
-                
-                <input
-                    type="email"
-                    placeholder={`${image} Enter your email address`}
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-                <button type="submit">Get Referral Link</button>
-            </form>
+            
+            {goodEmail ? (
+              <>
+                <span className={styles.success_message}>
+                  <img src="/success.svg" alt="success" width={"24px"} height={"24px"} />
+                  Your email is confirmed!
+                </span>
+                <div className={styles.referral_div}>
+                  <input
+                      type="text"
+                      defaultValue={referralLink}
+                      readOnly
+                  />
+                  <button onClick={copyLink}>Copy</button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className={styles.error_state}>{error && <div>{error}</div>}</div>
+                <form onSubmit={validateEmail}>
+                    <input
+                        type="email"
+                        placeholder={`${image} Enter your email address`}
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <button type="submit">Get Referral Link</button>
+                </form>
+              </>
+            )}
+
             <p className={styles.small_text}>Limits on max rewards apply.</p>
         </div>
     );
